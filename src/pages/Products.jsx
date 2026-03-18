f280a1131fb7f0972cb77d03f953aebfe1e7b583
asfahan09 <muhammadasfahan689@gmail.com>
2026-03-19 00:22:19 +0500

    layout2 name set

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Layout from "../components/layout/layout";
+import Layout from "../components/layout/Layout";
 import { IoIosArrowForward } from "react-icons/io";
 import { Link } from "react-router-dom";
 import productsimg1 from "../assets/product1.png";
