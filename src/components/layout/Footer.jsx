31c0db14569c2012f8e83d1f1f90a4df7c926bcc
asfahan09 <muhammadasfahan689@gmail.com>
2026-03-19 12:34:08 +0500

    add marding footer section on top

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,13 +4,13 @@ function Footer() {
   return (
     <footer>
       <div className="bg-green-300">
-        <div className="max-w-screen-lg px-4 sm:px-6 text-gray-800 sm:grid md:grid-cols-4 sm:grid-cols-2 mx-auto">
+        <div className="max-w-360  px-5 lg:px-8  text-gray-800 sm:grid md:grid-cols-4 sm:grid-cols-2 mx-auto">
           
-          <div className="p-5">
-            <h3 className="font-bold text-xl text-black">Company Name</h3>
+          <div className="py-5">
+            <h3 className="font-bold text-xl text-black">Ecommerce</h3>
           </div>
 
-          <div className="p-5">
+          <div className="py-5">
             <div className="text-sm uppercase text-black font-bold">
               Resources
             </div>
@@ -19,7 +19,7 @@ function Footer() {
             <a className="my-3 block" href="#">Support</a>
           </div>
 
-          <div className="p-5">
+          <div className="py-5">
             <div className="text-sm uppercase text-black font-bold">
               Support
             </div>
@@ -28,7 +28,7 @@ function Footer() {
             <a className="my-3 block" href="#">Conditions</a>
           </div>
 
-          <div className="p-5">
+          <div className="py-5">
             <div className="text-sm uppercase text-black font-bold">
               Contact us
             </div>
